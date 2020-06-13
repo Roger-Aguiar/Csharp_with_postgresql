9404b7b4e39d7c67021c80aa74c58e592dc22770
Roger-Aguiar <roger.silvaaguiar@yahoo.com.br>
2020-06-13 17:18:14 -0300

    My first commit, creating the project.

---
+++ MyApp/Program.cs
@@ -0,0 +1,19 @@
+﻿/*
+Name:         Roger Silva Santos Aguiar
+Function:     How to connect with Postgresql with C#
+Initial date: June 13, 2020
+Last update:  June 13, 2020
+*/
+using System;
+using Npgsql;
+
+namespace MyApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
